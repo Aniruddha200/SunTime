a18bb9795e93f43558e00b46819d7e08c039dc01
Aniruddha <aniruddha200baidya@protonmail.com>
2021-06-21 19:01:36 +0100

    Initial Commit

---
+++ SunTime/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SunTime
+//
+//  Created by administrator on 21/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SunTime/SunTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunTimeApp.swift
+//  SunTime
+//
+//  Created by administrator on 21/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SunTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
